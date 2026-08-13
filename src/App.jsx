b6bc3bca3ffad2d30b36e09cc6e0db9c0ad7dda8
kmiszop-jpg <kmiszop@gmail.com>
2026-08-13 08:59:39 +0700

    update ui again

--- src/App.jsx
+++ src/App.jsx
@@ -902,7 +902,7 @@ export default function App() {
 
           <div className="relative flex items-start gap-4">
 
-            <div className="w-10 h-10 rounded-xl bg-indigo-500/10 border border-indigo-400/20 flex items-center justify-center shrink-0">
+            <div className="w-10 h-10 rounded-xl bg-blue-500 border border-white flex items-center justify-center shrink-0">
 
               <AlertCircle className="w-5 h-5 text-indigo-300" />
 
@@ -939,7 +939,7 @@ export default function App() {
 
           <div className="flex items-center justify-center gap-2 mb-2">
 
-            <ShieldCheck className="w-4 h-4 text-indigo-400" />
+            <img src='/logo Smk Pertiwi 2.png'  className="w-4 h-5 text-indigo-400" />
 
             <span className="text-xs font-semibold text-slate-300">
               AI GUARDIAN
